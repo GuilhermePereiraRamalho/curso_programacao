5180a9e1a3739ff34b1f58eda3c9c716bf0169a5
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2025-10-15 20:54:17 +0200

    Convertendo data-hora para texto

--- src/DataHora.java
+++ src/DataHora.java
@@ -1,12 +1,16 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DataHora {
     public static void main(String[] args) {
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
@@ -34,5 +38,17 @@ public class DataHora {
         System.out.println("d09 = " + d09.toString());
         System.out.println("d10 = " + d10.toString());
         System.out.println("d11 = " + d11.toString());
+        System.out.println("-------------------------------------");
+
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d05 = " + d05.format(fmt4));
+
+        System.out.println("d06 = " + fmt3.format(d06));
+        System.out.println("d06 = " + fmt5.format(d06));
     }
 }
