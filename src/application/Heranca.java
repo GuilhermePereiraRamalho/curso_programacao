e2db2acfdfdeb87017df75bae80c8bed627c8ac9
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2025-10-21 20:33:06 +0200

    Herança

---
+++ src/application/Heranca.java
@@ -0,0 +1,9 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Heranca {
+    public static void main(String[] args) {
+        BusinessAccount account = new BusinessAccount();
+    }
+}
